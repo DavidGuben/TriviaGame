fc726396558b120d4e1203306c40e812341cef63
David Guben <david.guben@rutgers.edu>
2016-06-01 10:53:23 -0400

    fixing syntax and markup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,42 +1,70 @@
 
+//Object with trivia questions, options and answers.
+var triviaQA = [
+	{
+		"question": "Question 1",
+		"option1": "Option 1",
+		"option2": "Option 2",
+		"answer": "Answer",
+	},
+	{
+		"question": "Question 2",
+		"option1": "Option 1",
+		"option2": "Option 2",
+		"answer": "Answer",
+	},
+	{
+		"question": "Question 3",
+		"option1": "Option 1",
+		"option2": "Option 2",
+		"answer": "Answer",
+	},
+
+]
+
+//Timer start on button click
 $('#startTrivia').on("click", function() {
 
-//Timeouts for five/ten seconds and time up
-setTimeout(fiveSeconds, 1000 * 20);
-setTimeout(tenSeconds, 1000 * 25);
-setTimeout(timeUp, 1000 * 30);
+	//Styling timer and seconds left	
+	$('#secondsLeft').addClass('timerStyle');
+	$('#timer').addClass('timerStyle');
 
-//5 Seconds left function
-function fiveSeconds() {
-    $('#secondsLeft').html('<h2>About 10 Seconds Left!</h2>');
-}
+	//Timeouts for five/ten seconds and time up
+	setTimeout(fiveSeconds, 1000 * 20);
+	setTimeout(tenSeconds, 1000 * 25);
+	setTimeout(timeUp, 1000 * 30);
 
-//10 Seconds left function
-function tenSeconds() {
-    $('#secondsLeft').html('<h2>About 5 Seconds Left!</h2>');
-}
+	//5 Seconds left function
+	function fiveSeconds() {
+	    $('#secondsLeft').html('<h2>About 10 Seconds Left!</h2>');
+	}
 
-//Time up function
-function timeUp(){
-    $('#secondsLeft').html('<h2>Time\'s Up!</h2>');
-}
+	//10 Seconds left function
+	function tenSeconds() {
+	    $('#secondsLeft').html('<h2>About 5 Seconds Left!</h2>');
+	}
 
-//Timer variables
-var count = 30;
+	//Time up function
+	function timeUp(){
+	    $('#secondsLeft').html('<h2>Time\'s Up!</h2>');
+	}
 
-var counter = setInterval(timer, 1000); //1000 will run it every 1 second
+	//Timer variables
+	var count = 30;
 
-//Timer function counts down from 30 seconds
-function timer() {
+	var counter = setInterval(timer, 1000); //1000 will run it every 1 second
 
-  count = count - 1;
+	//Timer function counts down from 30 seconds
+	function timer() {
 
-  if (count == -1) {
-     clearInterval(counter); // Stops the counter
-     return;
-  }
+	  count = count - 1;
 
-  $("#timer").html(count + " secs");
-}
+	  if (count == -1) {
+	     clearInterval(counter); // Stops the counter
+	     return;
+	  }
+
+	  $("#timer").html(count + " secs");
+	}
 
 })
