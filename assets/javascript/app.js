35d5837e1ee326d3491110223011f13ef5701cc8
David Guben <david.guben@rutgers.edu>
2016-06-01 10:39:06 -0400

    added functioning countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,42 @@
-//Timer functions
-setTimeout(fiveSeconds, 1000 * 5);
-setTimeout(tenSeconds, 1000 * 10);
-setTimeout(timeUp, 1000 * 15);
 
+$('#startTrivia').on("click", function() {
+
+//Timeouts for five/ten seconds and time up
+setTimeout(fiveSeconds, 1000 * 20);
+setTimeout(tenSeconds, 1000 * 25);
+setTimeout(timeUp, 1000 * 30);
+
+//5 Seconds left function
 function fiveSeconds() {
-    $('#timeDisplay').html('<h2>About 10 Seconds Left!</h2>');
-    alert('10 seconds left');
+    $('#secondsLeft').html('<h2>About 10 Seconds Left!</h2>');
 }
 
+//10 Seconds left function
 function tenSeconds() {
-    $('#time-left').html('<h2>About 5 Seconds Left!</h2>');
-    alert('5 seconds left');
+    $('#secondsLeft').html('<h2>About 5 Seconds Left!</h2>');
 }
 
+//Time up function
 function timeUp(){
-    // audio.play();
-    console.log('done');
-    $('#time-left').html('<h2>Time\'s Up!</h2>');
-    alert('time is up');
+    $('#secondsLeft').html('<h2>Time\'s Up!</h2>');
 }
+
+//Timer variables
+var count = 30;
+
+var counter = setInterval(timer, 1000); //1000 will run it every 1 second
+
+//Timer function counts down from 30 seconds
+function timer() {
+
+  count = count - 1;
+
+  if (count == -1) {
+     clearInterval(counter); // Stops the counter
+     return;
+  }
+
+  $("#timer").html(count + " secs");
+}
+
+})
